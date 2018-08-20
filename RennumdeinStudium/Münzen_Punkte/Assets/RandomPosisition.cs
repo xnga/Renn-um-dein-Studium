ab819bd7a35e3c1cd57db575720b40c873e84b03
xnga <nga.ngo@hotmail.de>
2018-08-20 21:26:17 +0200

    Random Positionen

---
+++ RennumdeinStudium/Münzen_Punkte/Assets/RandomPosisition.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomPosisition : MonoBehaviour {
+    public Vector3[] positions; //[] für ein Array für die Position
+	// Use this for initialization
+	void Start () {
+        int randomNumber = Random.Range(0, positions.Length); // minimum,maximum
+        transform.position = positions[randomNumber];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
