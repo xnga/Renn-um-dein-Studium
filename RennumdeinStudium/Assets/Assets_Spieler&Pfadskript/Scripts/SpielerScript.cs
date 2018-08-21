33674e3462eeefa834305e8f369a303a0b2c3f0c
Mareike We <mareike@Mareikes-MacBook-Pro-2.local>
2018-08-21 21:01:15 +0200

    Cam & HindernisSpawn

--- RennumdeinStudium/Assets/Assets_Spieler&Pfadskript/Scripts/SpielerScript.cs
+++ RennumdeinStudium/Assets/Assets_Spieler&Pfadskript/Scripts/SpielerScript.cs
@@ -2,39 +2,54 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpielerScript : MonoBehaviour {
+public class SpielerScript : MonoBehaviour
+{
 
     public float speed = 10;
+    public float turnSpeed = 3;
     private Vector3 direction;
 
+<<<<<<< HEAD
 
-	// Use this for initialization
-	void Start () {
-        
+=======
+    float horizontal;
+
+>>>>>>> ade7df7024aafc4c240bc603979486de29cec4f6
+    // Use this for initialization
+    void Start()
+    {
+
         direction = Vector3.zero;       //Anfangspunkt
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        
-        if(Input.GetKeyDown(KeyCode.W)){
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        horizontal = Input.GetAxis("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
             direction = Vector3.forward;
         }
-        if(Input.GetKeyDown(KeyCode.S)){
+        if (Input.GetKeyDown(KeyCode.S))
+        {
             direction = Vector3.back;
         }
-        if(Input.GetKeyDown(KeyCode.D)){
-            direction = Vector3.right;
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(0, horizontal * turnSpeed, 0);
         }
-        if(Input.GetKeyDown(KeyCode.A)){
-            direction = Vector3.left;
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(0, horizontal * turnSpeed, 0);
         }
 
         float move = speed * Time.deltaTime;
 
         transform.Translate(direction * move);
-	}
+    }
 
 
 }
