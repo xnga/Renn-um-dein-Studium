72ed4fb887a7a16f2a6ae50b4efae0428c0a06b3
Mareike We <mareike@Mareikes-MacBook-Pro-2.local>
2018-08-22 14:55:46 +0200

    Wiederherstellung

---
+++ RennumdeinStudium/Assets/Healthbar/Health.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+    public const int maxHealth = 100;
+        public int currentHealth = maxHealth;
+
+    public void TakeDamage(int amount)
+    {
+        currentHealth -= amount;
+        if(currentHealth <= 0)
+        {
+            currentHealth = 0;
+            Debug.Log("Dead");
+        }
+    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
