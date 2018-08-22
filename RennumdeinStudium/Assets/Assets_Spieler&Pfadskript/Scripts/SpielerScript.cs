8f5969b59f3217af2d53459824fc7ec744a39462
xnga <nga.ngo@hotmail.de>
2018-08-22 10:25:57 +0200

    .

--- RennumdeinStudium/Assets/Assets_Spieler&Pfadskript/Scripts/SpielerScript.cs
+++ RennumdeinStudium/Assets/Assets_Spieler&Pfadskript/Scripts/SpielerScript.cs
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,10 +9,6 @@ public class SpielerScript : MonoBehaviour
     public float speed = 10;
     public float turnSpeed = 3;
     private Vector3 direction;
-
-
-    
-
     float horizontal;
 
 
@@ -57,61 +52,4 @@ public class SpielerScript : MonoBehaviour
 
 
 }
-=======
-=======
->>>>>>> ef39854e93b7acad6b41084ca17057f0266ad395
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
-public class SpielerScript : MonoBehaviour
-{
-
-    public float speed = 10;
-    public float turnSpeed = 3;
-    private Vector3 direction;
-
-    float horizontal;
-
-    // Use this for initialization
-    void Start()
-    {
-
-        direction = Vector3.zero;       //Anfangspunkt
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-        horizontal = Input.GetAxis("Horizontal");
-
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            direction = Vector3.forward;
-        }
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            direction = Vector3.back;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.Rotate(0, horizontal * turnSpeed, 0);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.Rotate(0, horizontal * turnSpeed, 0);
-        }
-
-        float move = speed * Time.deltaTime;
-
-        transform.Translate(direction * move);
-    }
-
-
-}
-<<<<<<< HEAD
->>>>>>> ef39854e93b7acad6b41084ca17057f0266ad395
-=======
->>>>>>> ef39854e93b7acad6b41084ca17057f0266ad395
