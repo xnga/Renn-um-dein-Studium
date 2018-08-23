c54ce0e2397f0d8bc8f64e2823ddb058a3144154
Mareike We <mareike@Mareikes-MacBook-Pro-2.local>
2018-08-23 22:03:21 +0200

    GameOver

--- RennumdeinStudium/Assets/scripts/Kollision.cs
+++ RennumdeinStudium/Assets/scripts/Kollision.cs
@@ -16,27 +16,8 @@ public class Kollision : MonoBehaviour
 
 	void Update()
 	{
-        if (playerDead == true) {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-        }
+        
 	}
-
-	void OnGUI() // von pattmann
-    {
-        GUI.color = Color.white;
-
-        /*if (HUDtime > 0)
-        {
-            HUDtime--;
-            GUI.Box(new Rect(Screen.width / 2, Screen.height / 2, 200, 100), "ESC to Quit\nR to Restart\nGOOD LUCK ;)", HUDStyle);
-        }*/
-
-        if (playerDead == true)
-        {
-            GUI.Box(new Rect(Screen.width / 2, Screen.height / 2, 200, 100), " Game Over \n Press R to Restart");
-        }
-
-    }
 
     void OnCollisionEnter(Collision colObject)
     {
