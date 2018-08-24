c30c1c3c795b0954c39a834945c431f222f323aa
Mareike We <mareike@Mareikes-MacBook-Pro-2.local>
2018-08-24 13:29:01 +0200

    Szenen Zusammenfuegen

--- RennumdeinStudium/Assets/Assets_Spieler&Pfadskript/Scripts/Pfad.cs
+++ RennumdeinStudium/Assets/Assets_Spieler&Pfadskript/Scripts/Pfad.cs
@@ -15,22 +15,10 @@ public class Pfad : MonoBehaviour
     public GameObject spawner;
     //public GameObject currentSpawner;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-    public Vector3 currentPfadPosition;
-
-   
-=======
-=======
->>>>>>> c54ce0e2397f0d8bc8f64e2823ddb058a3144154
     public Vector3 spawnerPos;
 
 
     private static Pfad instance;
-<<<<<<< HEAD
->>>>>>> c54ce0e2397f0d8bc8f64e2823ddb058a3144154
-=======
->>>>>>> c54ce0e2397f0d8bc8f64e2823ddb058a3144154
 
 
     public static Pfad Instance             //mit Pfad.Instance können alle Funktionen aus der Klasse angesprochen werden
@@ -50,33 +38,15 @@ public class Pfad : MonoBehaviour
     {
 
         for (int i = 0; i < 40; i++)
-        {      
+        {       //position z von player speichern und mit path vergleichen
             makePath();
-<<<<<<< HEAD
-=======
 
             HindernisSpawner.Instance.SpawnHindernisse();
 
 
->>>>>>> c54ce0e2397f0d8bc8f64e2823ddb058a3144154
-
-            HindernisSpawner.Instance.SpawnHindernisse();
-
-<<<<<<< HEAD
 
 
-
-<<<<<<< HEAD
         }
-
-
-
-=======
-        }
->>>>>>> c54ce0e2397f0d8bc8f64e2823ddb058a3144154
-=======
-        }
->>>>>>> c54ce0e2397f0d8bc8f64e2823ddb058a3144154
     }
 
     // Update is called once per frame
@@ -92,15 +62,6 @@ public class Pfad : MonoBehaviour
 
         currentPath = (GameObject)Instantiate(pathPrefabs[randomInd], currentPath.transform.GetChild(0).transform.GetChild(randomInd).position, Quaternion.identity);    //kopiert das Original und gibt die Kopie zurück && quaternion=natürliche Rot.
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-
-        //HindernisSpawner.Instance.StartRoutine();
-        //hindernis = GameObject.Find("Pfad").GetComponent<HindernisSpawner>();
-=======
-=======
->>>>>>> c54ce0e2397f0d8bc8f64e2823ddb058a3144154
         spawnerPos = new Vector3(currentPath.transform.position.x, currentPath.transform.position.y, currentPath.transform.position.z );
         Instantiate(spawner, currentPath.transform.position, Quaternion.identity);
 
@@ -113,19 +74,10 @@ public class Pfad : MonoBehaviour
             }*/
         //SpawnerPos = GameObject.Find("Spawner").GetComponent<HindernisSpawner>();
         //SpawnerPos.spawnwerte = prefabPos;
-<<<<<<< HEAD
->>>>>>> c54ce0e2397f0d8bc8f64e2823ddb058a3144154
-=======
->>>>>>> c54ce0e2397f0d8bc8f64e2823ddb058a3144154
 
      // Pos vom Spawner ändern >> spawnwerte == spawnerpos > noch nicht gegeben
         //currentSpawnPosition = GameObject.Find("Spawner").GetComponent<HindernisSpawner>();
         //currentSpawnPosition.spawnwerte = prefabPos;
 
     }
-
-
-
-         
-
 }
