c8953cf2aad37b6d1a6242bb779f7cf1a35f31ea
koerst666 <kirstin95@gmx.net>
2018-08-31 14:37:27 +0200

    Bewegung

--- RennumdeinStudium/Assets/Assets_Spieler&Pfadskript/Scripts/SpielerScript.cs
+++ RennumdeinStudium/Assets/Assets_Spieler&Pfadskript/Scripts/SpielerScript.cs
@@ -53,7 +53,7 @@ public class SpielerScript : MonoBehaviour {
 
         if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(0, horizontal * turnSpeed * Time.deltaTime, 0);
+            transform.Rotate (0, horizontal * turnSpeed * Time.deltaTime, 0);
         }
         else if (Input.GetKey(KeyCode.A))
         {
