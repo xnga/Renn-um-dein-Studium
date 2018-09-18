43e2be30370930879f7f459459f38f8f2b4e1456
xnga <nga.ngo@hotmail.de>
2018-09-18 19:05:16 +0200

    Health

--- RennumdeinStudium/Assets/Assets_Spieler&Pfadskript/Scripts/SpielerScript.cs
+++ RennumdeinStudium/Assets/Assets_Spieler&Pfadskript/Scripts/SpielerScript.cs
@@ -13,43 +13,11 @@ public class SpielerScript : MonoBehaviour
     public float jumpStrength = 5f;
     float horizontal;
     float vertical;
-
     public bool isGrounded = true;
     private float distToGround = 0f;
-
-
     Animator anim;
-
-    /* public float Health;
-     private float healthOverTime;   
-
-
-
-         public float Stamina;
-     public float staminaOverTime;
-     */
-
-
-
-
-    public float Hunger;
-    public float hungerOverTime;
-
-    public float Thirst;
-    public float thirstOverTime;
-
-
-    public Slider Hungerbar;
-    public Slider Thirstbar;
-    /*public Slider HealthBar;
-    public Slider StaminaBar;
-   
-    */
-
-
     public float minAmount = 5f;
     public float sprintSpeed = 5f;
-
     Rigidbody myBody;
 
 
@@ -59,16 +27,6 @@ public class SpielerScript : MonoBehaviour
         anim = GetComponent<Animator>();
 
         myBody = GetComponent<Rigidbody>();
-        Thirstbar.maxValue = Thirst;
-        Hungerbar.maxValue = Hunger;
-
-
-        /*HealthBar.maxValue = Health;
-        StaminaBar.maxValue = Stamina;
-        */
-
-        //updateUI();
-
 
     }
 
@@ -102,7 +60,6 @@ public class SpielerScript : MonoBehaviour
             anim.ResetTrigger("run");
         }
 
-
         if (Input.GetKey(KeyCode.D))
         {
             transform.Rotate(0, horizontal * turnSpeed * Time.deltaTime, 0);
@@ -126,88 +83,15 @@ public class SpielerScript : MonoBehaviour
             GetComponent<Rigidbody>().AddForce(0, jumpStrength, 0);                     //Stärke der Kraft hinzufügen
             anim.SetTrigger("Jump");
         }
-
-        CalculateValues();
-
+   
         if (isGrounded == false){
             anim.ResetTrigger("Jump");
         }
     }
-
-
-
 
     private void OnGUI()
     {
         GUI.Label(new Rect(10, 10, 100, 20), "Score:" + points);
     }
 
-    private void CalculateValues()
-    {
-        Hunger -= hungerOverTime * Time.deltaTime;
-        Thirst -= thirstOverTime * Time.deltaTime;
-
-        /*if(Hunger<= minAmount || Thirst <= minAmount)
-        {
-            Health -= healthOverTime * Time.deltaTime;
-            Stamina -= staminaOverTime * Time.deltaTime;
-
-        }
-        */
-
-        /*if(myBody.velocity.magnitude>=sprintSpeed && myBody.velocity.y == 0)
-        {
-            //Stamina -= staminaOverTime * Time.deltaTime;
-            Hunger -= hungerOverTime * Time.deltaTime;
-            Thirst -= thirstOverTime * Time.deltaTime;
-        }
-        /*else
-        {
-            Stamina += staminaOverTime * Time.deltaTime;
-
-        }*/
-
-        /*if (Health <= 0)
-        {
-            print("Player has died");
-        }*/
-
-
-        updateUI();
-
-    }
-
-    private void updateUI()
-    {
-        /*Thirstbar.maxValue = Thirst;
-        Hungerbar.maxValue = Hunger;
-
-        Health = Mathf.Clamp(Health, 0f, 100f);
-        Stamina = Mathf.Clamp(Health, 0f, 100f); */
-
-        Hunger = Mathf.Clamp(Hunger, 0f, 100f);
-        Thirst = Mathf.Clamp(Thirst, 0f, 100f);
-
-
-        Thirstbar.value = Thirst;
-        Hungerbar.value = Hunger;
-        /*HealthBar.value = Health;
-        StaminaBar.value = Stamina;*/
-
-
-    }
 }
-
-   /* public void TakeDamage(float amt)
-    {
-        Health -= amt;
-        updateUI();
-    }*/
-
-
-
-    /*private void OnGUI()
-    {
-        GUI.Label(new Rect(10, 10, 100, 20), "Score:" + points);
-    }
-*/
